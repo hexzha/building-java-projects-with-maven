524ba0dc3eae0687aa2d8a285faaf26286e02b00
Hexiang Zhang <sam10.zhang@gmail.com>
2019-08-27 00:29:06 -0500

    Incorporate localtime

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,7 +1,10 @@
 package main.java.hello;
 
+import org.joda.time.LocalTime;
+
 public class HelloWorld {
     public static void main(String [] args) {
+        LocalTime currentTime = new LocalTime();
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
     }
