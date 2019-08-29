4f9d4142ad05b3b9ee57ff13bdca41cee7914c76
Hexiang Zhang <sam10.zhang@gmail.com>
2019-08-29 00:34:45 -0500

    Move location for test

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -1,15 +1,12 @@
 package hello;
 
 import static org.hamcrest.CoreMatchers.containsString;
-package hello;
-
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class GreeterTest {
-
+	
 	private Greeter greeter = new Greeter();
 
 	@Test
